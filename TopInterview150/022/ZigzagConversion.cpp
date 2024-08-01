1ee32106d350731e9be50a87d429635c7335c788
witek-formanski <witekformanski@gmail.com>
2024-08-01 22:11:57 +0200

    add ZigZag

---
+++ TopInterview150/022/ZigzagConversion.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Solution {
+   public:
+    string convert(string s, int numRows) {
+        int length = s.length();
+        string* rows = new string[numRows];
+        int i = 0;
+
+        for (int j = 0; j < numRows; j++)
+            rows[j] = "";
+        
+        while (i < length) {
+            for (int j = 0; j < numRows && i < length; j++, i++)
+                rows[j] += s[i];
+            for (int j = numRows - 2; j > 0 && i < length; j--, i++)
+                rows[j] += s[i];
+        }
+
+        string result = "";
+
+        for (int j = 0; j < numRows; j++)
+            result += rows[j];
+
+        return result;
+    }
+};
